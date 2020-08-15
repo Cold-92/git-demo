8e7b4a9e7de764668b13f5d4ac308ca03eb15b78
DonnKeefer <201800995@qq.com>
2020-08-15 23:45:34 +0800

    项目自身配置

--- module-1/src/main/java/TestMain.java
+++ module-1/src/main/java/TestMain.java
@@ -2,7 +2,7 @@ public class TestMain {
 
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("chk");
+        System.out.println("chkasd");
     }
 
 }
