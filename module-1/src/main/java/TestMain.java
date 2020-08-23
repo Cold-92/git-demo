fa36cd04ef3919b70c661309e298b37bbbab9823
DonnKeefer <201800995@qq.com>
2020-08-23 23:59:23 +0800

    项目自身配置

--- module-1/src/main/java/TestMain.java
+++ module-1/src/main/java/TestMain.java
@@ -2,7 +2,7 @@ public class TestMain {
 
     public static void main(String[] args) {
         System.out.println("hasdgit");
-        System.out.println("ced");
+        System.out.println("asdd");
     }
 
 }
