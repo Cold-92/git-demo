cccf27be1b9ca3306d006622b60d9d1f0aaddfc3
DonnKeefer <201800995@qq.com>
2020-08-25 23:55:19 +0800

    项目自身配置

--- module-1/src/main/java/TestMain.java
+++ module-1/src/main/java/TestMain.java
@@ -1,7 +1,7 @@
 public class TestMain {
 
     public static void main(String[] args) {
-        System.out.println("asddgit");
+        System.out.println("asdasdasddgit");
         System.out.println("asdd");
     }
 
